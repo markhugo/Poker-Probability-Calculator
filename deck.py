7f5c1c10097398c768afc454346a0643ab643927
markhugo <markvincenthugo@gmail.com>
2024-09-08 02:13:26 -0700

    created Deck class

---
+++ deck.py
@@ -0,0 +1,30 @@
+# deck.py
+
+import random
+
+class Deck:
+    def __init__(self):
+        self.cards = self.create_deck()
+
+    # creates a standard deck of 52 cards
+    def create_deck(self):
+        suits = ['♠', '♥', '♦', '♣']
+        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+        return [rank + suit for rank in ranks for suit in suits]
+
+    # shuffles the deck
+    def shuffle_deck(deck):
+        random.shuffle(self.cards)
+    
+    # deals a specified number of cards from deck
+    def deal(self, num=2):
+        dealt_cards = self.cards[:num]
+        self.cards = self.cards[num:]
+        return dealt_cards
+
+    # resets the deck to a full 52 cards and shuffles it
+    def reset(self):
+        self.cards = self.create_deck()
+        self.shuffle
+
+    
